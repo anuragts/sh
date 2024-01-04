4eea1ca8508d4f3431f7d46d97d68770724b0665
Anurag Sharma <anuragsharma011011@gmail.com>
2024-01-04 11:11:29 +0000

    feat:mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import partytown from "@astrojs/partytown";
+import mdx from '@astrojs/mdx';
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), partytown({
+  integrations: [mdx(),tailwind(), partytown({
     // Adds dataLayer.push as a forwarding-event.
     config: {
       forward: ["dataLayer.push"],
